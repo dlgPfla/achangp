ada291d794252f2127ccf6675c942eba40230ddd
dlgPfla <132207550+dlgPfla@users.noreply.github.com>
2025-07-19 01:43:51 +0900

    🔥 remove tsc from build script

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/', // Netlify일 경우 보통 그대로 사용
+  build: {
+    outDir: 'dist' // ← 꼭 이렇게 되어 있어야 해요
+  }
 });
