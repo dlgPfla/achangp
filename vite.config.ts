d0b72a654c47d38f8aea30f894f86081035d00a9
dlgPfla <132207550+dlgPfla@users.noreply.github.com>
2025-07-19 01:21:29 +0900

    fix: remove tsc from build for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true, // 브라우저 자동 실행
-  },
+  plugins: [react()],
+  base: '/', // Netlify일 경우 보통 그대로 사용
 });
